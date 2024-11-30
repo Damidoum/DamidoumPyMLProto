92ff2119ce5b55c36d49ab2b84b7fccbcd9083dd
Rouchouse Damien <damien.rouchouse@etu.minesparis.psl.eu>
2024-11-30 23:40:33 +0100

    update example

--- example/ex1.py
+++ example/ex1.py
@@ -1,7 +1,9 @@
 from DamidoumPyMLProto import Tensor
+import numpy as np
 
 if __name__ == "__main__":
-    x = Tensor(data=2.0, requires_grad=True)
-    y = Tensor(data=3.0, requires_grad=True)
-    z = x + y
-    print(z)
+    x = Tensor(data=np.array([[2.0, 1.0], [2.0, 3.0]]), requires_grad=True)
+    w = Tensor(data=np.array([[3.0], [1.0]]), requires_grad=True)
+    b = Tensor(data=np.array([[4.0], [9.0]]), requires_grad=True)
+    l = x * w + b
+    print(l)
